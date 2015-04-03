6b62338b3aed1f3e20f5f23805a3c291bf6a9e8d
Karim Alibhai <k4rim.sa@gmail.com>
2015-04-03 17:37:24 -0400

    add string#startsWith()

--- index.js
+++ index.js
@@ -9,10 +9,23 @@
 "use strict";
 
 module.exports = {
+  // recursion utils
     R: {
       fn: require('./lib/R/fn.js')
     }
+
+  // some graph stuff
   , tree: require('./lib/graph/tree.js')
   , graph: require('./lib/graph/index.js')
+
+  // a useful tool to handle i/o for solution
+  // writing; see README.md
   , describe: require('./lib/io.js')
+
+  // adding these things to the prototype slows
+  // things down too much; and these are speedier
+  // implementations
+  , strings: {
+      startsWith: require('./lib/strings/startsWith.js')
+    }
 }
