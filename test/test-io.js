31ed2bf37e5f302c754e9794f03d16b5d9d0aca8
Karim Alibhai <k4rim.sa@gmail.com>
2015-04-05 14:12:08 -0400

    add variable input length

--- test/test-io.js
+++ test/test-io.js
@@ -9,21 +9,29 @@ test('test io', function (t) {
     try: 2,
     dir: __dirname,
     exit: false,
-    format: ['%d %w %f %s'],
-    fn: function (input, next) {
+    format: [
+      '%n',
+      '%d %w %f %s'
+    ],
+    fn: function (inputs, next) {
       // keep only first line
-      input = input[0]
+      var input, i
 
-      // test formatting
-      t.ok(true, 'reached logic')
-      t.equal(input.length, 4, 'proper input length')
-      t.equal(typeof input[0], 'number', 'first value is a number')
-      t.equal(typeof input[1], 'string', 'second value is a string')
-      t.equal(input[1].indexOf(' '), -1, 'second value has no spaces')
-      t.equal(typeof input[2], 'number', 'third value is a number')
-      t.notEqual(Math.floor(input[2]), input[2], 'third value is a float')
-      t.equal(typeof input[3], 'string', 'fourth value is a string')
-      t.notEqual(input[3].indexOf(' '), -1, 'fourth value has a space')
+      console.log(inputs)
+      for (i = 0; i < inputs.length; i += 1) {
+        input = inputs[i]
+
+        // test formatting
+        t.ok(true, 'reached logic')
+        t.equal(input.length, 4, 'proper input length')
+        t.equal(typeof input[0], 'number', 'first value is a number')
+        t.equal(typeof input[1], 'string', 'second value is a string')
+        t.equal(input[1].indexOf(' '), -1, 'second value has no spaces')
+        t.equal(typeof input[2], 'number', 'third value is a number')
+        t.notEqual(Math.floor(input[2]), input[2], 'third value is a float')
+        t.equal(typeof input[3], 'string', 'fourth value is a string')
+        t.notEqual(input[3].indexOf(' '), -1, 'fourth value has a space')
+      }
 
       next()
     }
